fe5f5ca542a2a4dab461e0daff3655c0b5baaa30
Nehal <nayneshpatel41118@gmail.com>
2024-06-14 10:26:49 +0530

    Commit

---
+++ lib/mange_product/add_product.dart
@@ -0,0 +1,70 @@
+import 'package:elements/constant/app_colors.dart';
+import 'package:elements/widget/custom_button.dart';
+import 'package:elements/widget/custom_text_field.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class AddProduct extends StatefulWidget {
+  const AddProduct({super.key});
+
+  @override
+  State<AddProduct> createState() => _AddProductState();
+}
+
+class _AddProductState extends State<AddProduct> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: const Color(0xffF9F9F9),
+          title: const Text(
+            "Add Product",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w400,
+              color: Colors.black,
+            ),
+          ),
+          leading: InkWell(
+              onTap: () {
+                Get.back();
+              },
+              child: const Icon(Icons.arrow_back_ios_new)),
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.add,
+                  size: 25,
+                ))
+          ],
+        ),
+        body: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+          child: const Column(
+            children: [
+              CustomTextField(
+                hintText: "Steel",
+                labelText: "Name*",
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              CustomTextField(
+                hintText: "20",
+                labelText: "Qty*",
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          child: CustomButton(
+            color: AppColor.buttonColor,
+            buttonText: 'Add',
+            onTap: () {},
+          ),
+        ));
+  }
+}
---
+++ lib/mange_product/mange_product.dart
@@ -0,0 +1,155 @@
+import 'package:elements/constant/app_colors.dart';
+import 'package:elements/mange_product/add_product.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+
+class MangeProduct extends StatefulWidget {
+  const MangeProduct({super.key});
+
+  @override
+  State<MangeProduct> createState() => _MangeProductState();
+}
+
+class _MangeProductState extends State<MangeProduct> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: const Color(0xffF9F9F9),
+        title: const Text(
+          "Product Stock",
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w400,
+            color: Colors.black,
+          ),
+        ),
+        leading: InkWell(
+            onTap: () {
+              Get.back();
+            },
+            child: const Icon(Icons.arrow_back_ios_new)),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Row(
+                  children: [
+                    Text(
+                      "Total Product Import : ",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w400, fontSize: 16),
+                    ),
+                    Text(
+                      "30",
+                      style: TextStyle(
+                        color: Color(0xff555555),
+                      ),
+                    ),
+                  ],
+                ),
+                InkWell(
+                  child: Container(
+                      height: 38,
+                      width: 96,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(4),
+                          border: Border.all(color: const Color(0xffC9C9C9))),
+                      child: const Center(
+                        child: Text(
+                          "Update",
+                          style: TextStyle(
+                            color: AppColor.buttonColor,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                      )),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: 4,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  contentPadding: const EdgeInsets.all(0),
+                  title: const Text(
+                    'Product 1',
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                        color: Color(0xff555555)),
+                  ),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Container(
+                        height: 44,
+                        width: 115,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(4),
+                            border: Border.all(color: const Color(0xffD1D1D1))),
+                        child: const TextField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              contentPadding: EdgeInsets.symmetric(
+                                horizontal: 25,
+                              ),
+                              hintText: '20 piece',
+                              hintStyle: TextStyle(
+                                  color: Color(0xff949494),
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w400)),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 12,
+                      ),
+                      InkWell(
+                        borderRadius: BorderRadius.circular(10),
+                        onTap: () {},
+                        child: Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(4),
+                                border:
+                                    Border.all(color: const Color(0xffD1D1D1))),
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 12, horizontal: 12),
+                            child: SvgPicture.asset(
+                              'assets/svg/ic_edit.svg',
+                              height: 16,
+                              width: 16,
+                            )),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          elevation: 10.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(36),
+          ),
+          backgroundColor: AppColor.buttonColor,
+          onPressed: () {
+            Get.to(const AddProduct());
+          },
+          child: const Icon(
+            Icons.add,
+            color: Colors.white,
+          )),
+    );
+  }
+}
