f5933f1686c8dcaffd7b6162cc0ab512f0beddf1
Nehal <nayneshpatel41118@gmail.com>
2024-06-18 17:18:20 +0530

    Commit

--- lib/spareparts/spareparts_details.dart
+++ lib/spareparts/spareparts_details.dart
@@ -1,5 +1,5 @@
-import 'package:data_table_2/data_table_2.dart';
 import 'package:elements/constant/app_colors.dart';
+import 'package:elements/constant/app_text_style.dart';
 import 'package:elements/widget/app%20bar/custom_appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -40,63 +40,143 @@ class _SparepartsDetailsState extends State<SparepartsDetails> {
         ),
         body: Container(
           margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-          child: DataTable2(
-            dividerThickness: 0.2,
-            columnSpacing: 20,
-            horizontalMargin: 5,
-            minWidth: 400,
-            headingRowColor: MaterialStateProperty.all<Color>(
-                const Color(0xffF1F1F1).withOpacity(0.60)),
-            // dataRowColor: MaterialStateProperty.all<Color>(Colors.white),
-            dataTextStyle: const TextStyle(color: Color(0xff555555)),
-            columns: [
-              const DataColumn2(
-                label: Text(
-                  'Name',
-                  style: TextStyle(
-                    color: AppColor.buttonColor,
-                  ),
-                ),
-                size: ColumnSize.L,
+          child: Column(
+            children: [
+              _tableTopContent(),
+              _tableView(
+                name: 'Steel Bolt',
+                date: "5/10/2023",
+                qty: '20',
               ),
-              DataColumn(
-                label: Row(
-                  children: [
-                    const Text(
-                      'Date',
-                      style: TextStyle(
-                        color: AppColor.buttonColor,
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 5,
-                    ),
-                    InkWell(
-                      onTap: () {
-                        Get.to(const DatePiker());
-                      },
-                      child: Image.asset(
-                        "assets/images/date.png",
-                        height: 14,
-                        width: 14,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const DataColumn(
-                label: Text('Qty'),
+              _tableView(
+                name: 'Steel Paip',
+                date: "5/10/2023",
+                qty: '50',
               ),
             ],
-            rows: List<DataRow>.generate(
-              2,
-              (index) => const DataRow(cells: [
-                DataCell(Text('Steel Bolt')),
-                DataCell(Text('5/10/2023')),
-                DataCell(Text('20')),
-              ]),
+          ),
+          // child: DataTable2(
+          //   dividerThickness: 0.2,
+          //   columnSpacing: 20,
+          //   horizontalMargin: 5,
+          //   minWidth: 400,
+          //   headingRowColor: MaterialStateProperty.all<Color>(
+          //       const Color(0xffF1F1F1).withOpacity(0.60)),
+          //   // dataRowColor: MaterialStateProperty.all<Color>(Colors.white),
+          //   dataTextStyle: const TextStyle(color: Color(0xff555555)),
+          //   columns: [
+          //     const DataColumn2(
+          //       label: Text(
+          //         'Name',
+          //         style: TextStyle(
+          //           color: AppColor.buttonColor,
+          //         ),
+          //       ),
+          //       size: ColumnSize.L,
+          //     ),
+          //     DataColumn(
+          //       label: Row(
+          //         children: [
+          //           const Text(
+          //             'Date',
+          //             style: TextStyle(
+          //               color: AppColor.buttonColor,
+          //             ),
+          //           ),
+          //           const SizedBox(
+          //             width: 5,
+          //           ),
+          //           InkWell(
+          //             onTap: () {
+          //               Get.to(const DatePiker());
+          //             },
+          //             child: Image.asset(
+          //               "assets/images/date.png",
+          //               height: 14,
+          //               width: 14,
+          //             ),
+          //           ),
+          //         ],
+          //       ),
+          //     ),
+          //     const DataColumn(
+          //       label: Text('Qty'),
+          //     ),
+          //   ],
+          //   rows: List<DataRow>.generate(
+          //     2,
+          //     (index) => const DataRow(cells: [
+          //       DataCell(Text('Steel Bolt')),
+          //       DataCell(Text('5/10/2023')),
+          //       DataCell(Text('20')),
+          //     ]),
+          //   ),
+          // ),
+        ));
+  }
+
+  Widget _tableView({required String qty, name, date}) {
+    return Column(
+      children: [
+        const SizedBox(
+          height: 12.0,
+        ),
+        Row(
+          children: [
+            Expanded(child: Text(name, style: AppTextStyle.textStyleRegular13)),
+            Expanded(
+                child: Text("$date", style: AppTextStyle.textStyleRegular13)),
+            Text(qty, style: AppTextStyle.textStyleRegular13)
+          ],
+        ),
+        const SizedBox(
+          height: 8.0,
+        ),
+        const Divider(),
+      ],
+    );
+  }
+
+  Widget _tableTopContent() {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
+      decoration: const BoxDecoration(color: Color(0xffF1F1F1)),
+      child: Row(
+        children: [
+          Expanded(
+              child: Text(
+            "Name",
+            style: AppTextStyle.textStyleRegular14
+                .copyWith(color: AppColor.selectColor),
+          )),
+          Expanded(
+            child: InkWell(
+              onTap: () {
+                Get.to(const DatePiker());
+              },
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "Date",
+                    style: AppTextStyle.textStyleRegular14
+                        .copyWith(color: AppColor.selectColor),
+                  ),
+                  const SizedBox(
+                    width: 2.0,
+                  ),
+                  Image.asset(
+                    "assets/images/date.png",
+                    height: 14,
+                    width: 14,
+                  ),
+                ],
+              ),
             ),
           ),
-        ));
+          Text("Authoriy", style: AppTextStyle.textStyleRegular14),
+        ],
+      ),
+    );
   }
 }
--- lib/user/mange_user.dart
+++ lib/user/mange_user.dart
@@ -1,8 +1,10 @@
-import 'package:data_table_2/data_table_2.dart';
 import 'package:elements/constant/app_colors.dart';
 import 'package:elements/user/add_user.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import '../constant/app_text_style.dart';
+import '../date_piker.dart';
 
 class MangeUser extends StatefulWidget {
   const MangeUser({super.key});
@@ -35,61 +37,81 @@ class _MangeUserState extends State<MangeUser> {
       ),
       body: Container(
         margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-        child: DataTable2(
-          dividerThickness: 0.2,
-          columnSpacing: 20,
-          horizontalMargin: 5,
-          minWidth: 400,
-          headingRowColor: MaterialStateProperty.all<Color>(
-              const Color(0xffF1F1F1).withOpacity(0.60)),
-          // dataRowColor: MaterialStateProperty.all<Color>(Colors.white),
-          dataTextStyle: const TextStyle(color: Color(0xff555555)),
-          columns: [
-            const DataColumn2(
-              label: Text(
-                'Name',
-                style: TextStyle(
-                  color: AppColor.buttonColor,
-                ),
-              ),
-              size: ColumnSize.L,
+        child: Column(
+          children: [
+            _tableTopContent(),
+            _tableView(
+              name: 'Dipesh',
+              date: "5/10/2023",
+              Authoriy: 'Worker',
             ),
-            DataColumn(
-              label: Row(
-                children: [
-                  const Text(
-                    'Date',
-                    style: TextStyle(
-                      color: AppColor.buttonColor,
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 5,
-                  ),
-                  InkWell(
-                    onTap: () {},
-                    child: Image.asset(
-                      "assets/images/date.png",
-                      height: 14,
-                      width: 14,
-                    ),
-                  ),
-                ],
-              ),
+            _tableView(
+              name: 'Dipesh',
+              date: "5/10/2023",
+              Authoriy: 'Worker',
             ),
-            const DataColumn(
-              label: Text('Authority'),
+            _tableView(
+              name: 'Dipesh',
+              date: "5/10/2023",
+              Authoriy: 'Helper',
             ),
           ],
-          rows: List<DataRow>.generate(
-            3,
-            (index) => const DataRow(cells: [
-              DataCell(Text('Dipesh')),
-              DataCell(Text('5/10/2023')),
-              DataCell(Text('Worker')),
-            ]),
-          ),
         ),
+        // child: DataTable2(
+        //   dividerThickness: 0.2,
+        //   columnSpacing: 20,
+        //   horizontalMargin: 5,
+        //   minWidth: 400,
+        //   headingRowColor: MaterialStateProperty.all<Color>(
+        //       const Color(0xffF1F1F1).withOpacity(0.60)),
+        //   // dataRowColor: MaterialStateProperty.all<Color>(Colors.white),
+        //   dataTextStyle: const TextStyle(color: Color(0xff555555)),
+        //   columns: [
+        //     const DataColumn2(
+        //       label: Text(
+        //         'Name',
+        //         style: TextStyle(
+        //           color: AppColor.buttonColor,
+        //         ),
+        //       ),
+        //       size: ColumnSize.L,
+        //     ),
+        //     DataColumn(
+        //       label: Row(
+        //         children: [
+        //           const Text(
+        //             'Date',
+        //             style: TextStyle(
+        //               color: AppColor.buttonColor,
+        //             ),
+        //           ),
+        //           const SizedBox(
+        //             width: 5,
+        //           ),
+        //           InkWell(
+        //             onTap: () {},
+        //             child: Image.asset(
+        //               "assets/images/date.png",
+        //               height: 14,
+        //               width: 14,
+        //             ),
+        //           ),
+        //         ],
+        //       ),
+        //     ),
+        //     const DataColumn(
+        //       label: Text('Authority'),
+        //     ),
+        //   ],
+        //   rows: List<DataRow>.generate(
+        //     3,
+        //     (index) => const DataRow(cells: [
+        //       DataCell(Text('Dipesh')),
+        //       DataCell(Text('5/10/2023')),
+        //       DataCell(Text('Worker')),
+        //     ]),
+        //   ),
+        // ),
       ),
       floatingActionButton: FloatingActionButton(
           elevation: 10.0,
@@ -104,6 +126,72 @@ class _MangeUserState extends State<MangeUser> {
             Icons.add,
             color: Colors.white,
           )),
+    );
+  }
+
+  Widget _tableView(
+      {required String Authoriy, name, date, bool isWeeklyOff = false}) {
+    return Column(
+      children: [
+        const SizedBox(
+          height: 12.0,
+        ),
+        Row(
+          children: [
+            Expanded(child: Text(name, style: AppTextStyle.textStyleRegular13)),
+            Expanded(
+                child: Text("$date", style: AppTextStyle.textStyleRegular13)),
+            Text(Authoriy, style: AppTextStyle.textStyleRegular13)
+          ],
+        ),
+        const SizedBox(
+          height: 8.0,
+        ),
+        const Divider(),
+      ],
+    );
+  }
+
+  Widget _tableTopContent() {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
+      decoration: const BoxDecoration(color: Color(0xffF1F1F1)),
+      child: Row(
+        children: [
+          Expanded(
+              child: Text(
+            "Name",
+            style: AppTextStyle.textStyleRegular14
+                .copyWith(color: AppColor.selectColor),
+          )),
+          Expanded(
+            child: InkWell(
+              onTap: () {
+                Get.to(const DatePiker());
+              },
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "Date",
+                    style: AppTextStyle.textStyleRegular14
+                        .copyWith(color: AppColor.selectColor),
+                  ),
+                  const SizedBox(
+                    width: 2.0,
+                  ),
+                  Image.asset(
+                    "assets/images/date.png",
+                    height: 14,
+                    width: 14,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Text("Authoriy", style: AppTextStyle.textStyleRegular14),
+        ],
+      ),
     );
   }
 }
