bb075199c70f6e8f8c159cbfc9691105719d7cea
Nehal <nayneshpatel41118@gmail.com>
2024-06-27 12:28:51 +0530

    Commit

--- lib/controller/user_controller.dart
+++ lib/controller/user_controller.dart
@@ -1,9 +1,15 @@
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 
 class UserController extends GetxController {
+  TextEditingController userNameTextEditingController = TextEditingController();
+  TextEditingController contactNoTextEditingController =
+      TextEditingController();
+  TextEditingController addressTextEditingController = TextEditingController();
+
   File? imgFile;
 
   Future<bool> pickImageFromGallery() async {
