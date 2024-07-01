914ec28627ac863d67ca7b1e8b47085bf2f3f73e
Fenil <femilpatel@gmail.com>
2024-07-01 11:28:14 +0530

    commit

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -18,121 +18,129 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   AuthController controller = Get.find();
-
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: AppColor.whiteColor,
-        body: ListView(
-          children: [
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(
-                    height: 60,
-                  ),
-                  Center(
-                    child: Image.asset(
-                      "assets/images/logo.png",
-                      height: 70,
-                      width: 70,
+        body: Form(
+          key: _formKey,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          child: ListView(
+            children: [
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      height: 60,
                     ),
-                  ),
-                  const SizedBox(
-                    height: 40,
-                  ),
-                  Text("Login",
-                      style: AppTextStyle.textStyleBold20
-                          .copyWith(fontSize: 26, fontFamily: "Kanit")),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  WidgetDropDownFromField(
-                    hintText: "Select User Type",
-                    labelText: "User Type*",
-                    itemList: const ["Admin", "Manager", "Salesman"],
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return "Enter user type";
-                      } else {
-                        return null;
-                      }
-                    },
-                    onTap: (value) {
-                      controller.userTypeTextEditingController.text = value;
-                      debugPrint("Select => $value");
-                    },
-                  ),
-                  verticalSpacing(),
-                  CustomTextField(
-                    focusNode: controller.mobileFocusNode,
-                    textInputType: TextInputType.number,
-                    textEditingController:
-                        controller.mobileNumberTextEditingController,
-                    maxLength: 10,
-                    hintText: "Mobile No",
-                    labelText: "Mobile No*",
-                  ),
-                  verticalSpacing(),
-                  CustomTextField(
-                      focusNode: controller.passwordFocusNode,
-                      obscureText: controller.loginPasswordVisible,
+                    Center(
+                      child: Image.asset(
+                        "assets/images/logo.png",
+                        height: 70,
+                        width: 70,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    Text("Login",
+                        style: AppTextStyle.textStyleBold20
+                            .copyWith(fontSize: 26, fontFamily: "Kanit")),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    WidgetDropDownFromField(
+                      hintText: "Select User Type",
+                      labelText: "User Type*",
+                      itemList: const ["Admin", "Manager", "Salesman"],
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Enter user type";
+                        } else {
+                          return null;
+                        }
+                      },
+                      onTap: (value) {
+                        controller.userTypeTextEditingController.text = value;
+                        debugPrint("Select => $value");
+                      },
+                    ),
+                    verticalSpacing(),
+                    CustomTextField(
+                      focusNode: controller.mobileFocusNode,
+                      textInputType: TextInputType.number,
                       textEditingController:
-                          controller.passwordTextEditingController,
-                      maxLength: 6,
-                      hintText: "Password",
-                      labelText: "Password*",
-                      suffixFixIcon: IconButton(
-                        onPressed: () {
-                          setState(() {
-                            controller.loginPasswordVisible =
-                                !controller.loginPasswordVisible;
-                          });
+                          controller.mobileNumberTextEditingController,
+                      maxLength: 10,
+                      hintText: "Mobile No",
+                      labelText: "Mobile No*",
+                      validator: (value) {
+                        if(value!.isEmpty){
+                          return "Enter Mobile No*";
+                        }
+                        else{
+                          return null;
+                        }
+                      },
+                    ),
+                    verticalSpacing(),
+                    CustomTextField(
+                        focusNode: controller.passwordFocusNode,
+                        obscureText: controller.loginPasswordVisible,
+                        textEditingController:
+                            controller.passwordTextEditingController,
+                        maxLength: 6,
+                        hintText: "Password",
+                        labelText: "Password*",
+                        validator: (value) {
+                          if(value!.isEmpty){
+                            return "Enter Password*";
+                          }
+                          else{
+                            return null;
+                          }
                         },
-                        icon: Icon(
-                          controller.loginPasswordVisible
-                              ? Icons.visibility
-                              : Icons.visibility_off,
-                          size: 20,
-                        ),
-                      )),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Obx(
-                    () => controller.isLoginLoading.value
-                        ? const CustomButtonLoader()
-                        : CustomButton(
-                            color: AppColor.buttonColor,
-                            buttonText: 'Login',
-                            onTap: () {
-                              if (controller.mobileNumberTextEditingController
-                                  .text.isEmpty) {
-                                showToast("Please enter mobile number");
-                              }
-                              if (controller
-                                  .passwordTextEditingController.text.isEmpty) {
-                                showToast("Please enter password number");
-                              } else if (!isValidPhoneNumber(controller
-                                  .mobileNumberTextEditingController.text)) {
-                                showToast("Please enter valid mobile number");
-                                if (controller.userTypeTextEditingController
-                                    .text.isEmpty) {
-                                  showToast("Please enter user type");
-                                }
-                              } else {
-                                controller.login();
-                              }
-                            },
+                        suffixFixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              controller.loginPasswordVisible =
+                                  !controller.loginPasswordVisible;
+                            });
+                          },
+                          icon: Icon(
+                            controller.loginPasswordVisible
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                            size: 20,
                           ),
-                  ),
-                ],
+                        )),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Obx(
+                      () => controller.isLoginLoading.value
+                          ? const CustomButtonLoader()
+                          : CustomButton(
+                              color: AppColor.buttonColor,
+                              buttonText: 'Login',
+                              onTap: () {
+                                if (_formKey.currentState!.validate()){
+
+                                  controller.login();
+
+                                }
+                              },
+                            ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ));
   }
 
