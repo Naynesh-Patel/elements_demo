95feb640a2f227b0d4b5224dc5674bc83e31cf79
naynesh <nayneshpatel4118@gmail.com>
2024-08-09 04:52:05 -0700

    commit

--- lib/machinery/view_machinery.dart
+++ lib/machinery/view_machinery.dart
@@ -5,6 +5,8 @@ import 'package:elements/widget/app%20bar/custom_appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../constant/app_colors.dart';
+
 class ViewMachinery extends StatefulWidget {
   final Map<String, dynamic> model;
 
@@ -38,44 +40,98 @@ class _ViewMachineryState extends State<ViewMachinery> {
       body: ListView(
         shrinkWrap: true,
         children: [
-          viewTile(
-              title: "Machine Name", subtitle: widget.model['machine_name']),
-          viewTile(
-              title: "Machine Type", subtitle: widget.model['machine_type']),
-          viewTile(
-              title: "Qty", subtitle: widget.model['manufacture_duration']),
-          viewTile(
-              title: "Manufacture Days",
-              subtitle: widget.model['manufacture_duration']),
-          const SizedBox(height: 10.0,),
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Text(
-              "Spareparts ",
-              style: AppTextStyle.textStyleRegular18.copyWith(backgroundColor: Colors.grey.withOpacity(0.3)),
-            ),
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 15,vertical: 15),
+          child:   Column(
+            // shrinkWrap: true,
+            children: [
+              buildMenu(
+                title: "Machine Name",
+                subTitle: widget.model['Machine Name'] ?? '',
+              ),
+              buildMenu(
+                title: "Machine Type",
+                subTitle: widget.model['machine_type'] ?? '',
+              ),
+              buildMenu(
+                title: "Qty",
+                subTitle: widget.model['manufacture_duration'] ?? '',
+              ),
+            ],
           ),
-          const SizedBox(height: 10.0,),
-          ListView.separated(
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            itemCount: sparepartsList.length,
-            itemBuilder: (context, index) {
-              return  Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  _keyValue( "Name",sparepartsList[index]['name'] ?? ''),
-                  _keyValue( "Qty",sparepartsList[index]['qty'] ?? ''),
-                ],
-              );
-            }, separatorBuilder: (BuildContext context, int index) {
-            return const SizedBox(height: 15,);
+        ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Text(
+                  "Spareparts ",
+                  style: AppTextStyle.textStyleRegular18.copyWith(backgroundColor: Colors.grey.withOpacity(0.3)),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              ListView.separated(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                itemCount: sparepartsList.length,
+                itemBuilder: (context, index) {
+                  return  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      _keyValue( "Name",sparepartsList[index]['name'] ?? ''),
+                      _keyValue( "Qty",sparepartsList[index]['qty'] ?? ''),
+                    ],
+                  );
+                }, separatorBuilder: (BuildContext context, int index) {
+                return const SizedBox(height: 15,);
+              },
+              ),
+            ],
+          ),
 
-          },
-          ),
-        ],
-      ),
+
+      // body: ListView(
+      //   shrinkWrap: true,
+      //   children: [
+      //     viewTile(
+      //         title: "Machine Name", subtitle: widget.model['Machine Name']),
+      //     viewTile(
+      //         title: "Machine Type", subtitle: widget.model['machine_type']),
+      //     viewTile(
+      //         title: "Qty", subtitle: widget.model['manufacture_duration']),
+      //     viewTile(
+      //         title: "Manufacture Days",
+      //         subtitle: widget.model['manufacture_duration']),
+      //     const SizedBox(height: 10.0,),
+      //     Container(
+      //       padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      //       child: Text(
+      //         "Spareparts ",
+      //         style: AppTextStyle.textStyleRegular18.copyWith(backgroundColor: Colors.grey.withOpacity(0.3)),
+      //       ),
+      //     ),
+      //     const SizedBox(height: 10.0,),
+      //     ListView.separated(
+      //       shrinkWrap: true,
+      //       physics: const NeverScrollableScrollPhysics(),
+      //       padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      //       itemCount: sparepartsList.length,
+      //       itemBuilder: (context, index) {
+      //         return  Column(
+      //           crossAxisAlignment: CrossAxisAlignment.start,
+      //           children: [
+      //             _keyValue( "Name",sparepartsList[index]['name'] ?? ''),
+      //             _keyValue( "Qty",sparepartsList[index]['qty'] ?? ''),
+      //           ],
+      //         );
+      //       }, separatorBuilder: (BuildContext context, int index) {
+      //       return const SizedBox(height: 15,);
+      //
+      //     },
+      //     ),
+      //   ],
+      // ),
     );
   }
 
@@ -116,6 +172,25 @@ class _ViewMachineryState extends State<ViewMachinery> {
   Widget verticalSpacing() {
     return const SizedBox(
       height: 6.0,
+    );
+  }
+
+
+
+  Widget buildMenu({required String title, required String subTitle}) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const SizedBox(height: 6.0,),
+        Text(title,style: AppTextStyle.textStyleRegular12,),
+        const SizedBox(height: 6.0,),
+        Text(subTitle,style: AppTextStyle.textStyleRegular16,),
+        const SizedBox(height: 6.0,),
+        const Divider(
+          color: AppColor.borderColor,
+        ),
+      ],
     );
   }
 
